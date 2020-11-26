dc309841b313f5772d4460d0feafbea796f786b6
Theodore R. Smith <theodore@phpexperts.pro>
2020-11-26 02:39:36 -0600

    [api] Added the ForgotPassword React.js page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,9 @@ class App extends React.Component {
                 <div>
                     <h1>MindStream Journal</h1>
                     <div><img src="/logo.png" title="MindStream Journal" alt="MindStream Journal" width="250" height="250" /></div>
+                    <nav>
+                        <a href="/">Home</a> | <a href="/forgot-password">Forgot Password</a>
+                    </nav>
                 </div>
             </section>
         );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,12 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './App';
 import './index.css';
+import ForgotPasswordPage from './components/auth/ForgotPassword';
 
 ReactDOM.render((
     <BrowserRouter>
         <Route path = "/" component = {App}/>
+        <Route path = "/forgot-password" component = {ForgotPasswordPage} />
         {/*<Route path = "home" component = {Home} />*/}
         {/*<Route path = "about" component = {About} />*/}
         {/*<Route path = "contact" component = {Contact} />*/}
