d060567d5f167430b24633fe3973d235d3c382eb
Theodore R. Smith <theodore@phpexperts.pro>
2020-12-02 06:51:12 -0600

    [api] Added Middleware to Authorize users via JWT.

---
+++ backend/middleware/myJWT.js
@@ -0,0 +1,27 @@
+const myJWT = require("jsonwebtoken");
+
+const authenticate = (request, response, next) => {
+    // Gather the jwt access token from the request header
+    const authHeader = request.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null)
+    {
+        return response.sendStatus(401);
+    }
+
+    myJWT.verify(token, process.env.JWT_SECRET, (error, user) => {
+        console.log(error);
+        if (error) {
+            return response.status(403).json({
+                status: false,
+                error
+            });
+        }
+        request.user = user;
+        next();
+    });
+};
+
+module.exports = {
+    authenticate
+};
