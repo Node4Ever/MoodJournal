b4faaa82660336a6a8d6e67b69e8f812964c9db4
Theodore R. Smith <theodore@phpexperts.pro>
2020-12-02 09:14:43 -0600

    [api] Return when the JWT token will expire.

--- backend/services/myJWT.js
+++ backend/services/myJWT.js
@@ -5,13 +5,15 @@ const generate = async function(userId, email) {
         throw new Error('AuthToken requires a user ID')
     }
 
+    const expiresIn = parseInt(process.env.JWT_TTL) * 60;
     const token = jwt.sign({ sub: email }, process.env.JWT_SECRET, {
-        expiresIn: parseInt(process.env.JWT_TTL) * 60
+        expiresIn: expiresIn
     });
 
     return {
         token: token,
-        createdAt: (new Date()).toISOString()
+        createdAt: (new Date()).toISOString(),
+        expiresAt: (new Date(new Date().getTime() + expiresIn * 1000))
     };
 };
 
